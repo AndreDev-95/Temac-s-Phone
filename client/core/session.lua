6832a58a582807fda24182ee5e5db67b5cbb0a8b
AndreDev-95 <andredeveloper1995@gmail.com>
2026-04-06 17:58:15 +0100

    feat(core): adiciona memória temporária de sessão

---
+++ client/core/session.lua
@@ -0,0 +1,20 @@
+-- Session Memory
+
+PhoneSession = {}
+PhoneSession.values = {}
+
+function PhoneSession:SetValue(key, value)
+    self.values[key] = value
+end
+
+function PhoneSession:GetValue(key)
+    return self.values[key]
+end
+
+function PhoneSession:DeleteValue(key)
+    self.values[key] = nil
+end
+
+function PhoneSession:ClearAll()
+    self.values = {}
+end
