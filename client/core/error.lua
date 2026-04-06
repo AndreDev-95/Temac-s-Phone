59df0dacc16778c9610cb88fddcfb985979d436d
AndreDev-95 <andredeveloper1995@gmail.com>
2026-04-06 17:53:11 +0100

    feat(core): adiciona sistema de tratamento de erros global

---
+++ client/core/error.lua
@@ -0,0 +1,14 @@
+-- Error Handler
+
+PhoneError = {}
+
+function PhoneError:Handle(err)
+    print('[TEMAC PHONE ERROR]: ' .. tostring(err))
+end
+
+function SafeExecute(fn)
+    local success, err = pcall(fn)
+    if not success then
+        PhoneError:Handle(err)
+    end
+end
