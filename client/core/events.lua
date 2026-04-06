b2034a23420a2ab663ba26f3c7d8451887072ff4
AndreDev-95 <andredeveloper1995@gmail.com>
2026-04-06 17:51:55 +0100

    feat(core): adiciona sistema de eventos (event bus)

---
+++ client/core/events.lua
@@ -0,0 +1,18 @@
+-- Event Bus
+
+PhoneEvents = {}
+PhoneEvents.listeners = {}
+
+function PhoneEvents:On(event, callback)
+    if not self.listeners[event] then
+        self.listeners[event] = {}
+    end
+    table.insert(self.listeners[event], callback)
+end
+
+function PhoneEvents:Emit(event, data)
+    if not self.listeners[event] then return end
+    for _, cb in pairs(self.listeners[event]) do
+        cb(data)
+    end
+end
